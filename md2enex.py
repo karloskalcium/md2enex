1ec547022cfe69f0fb315e4671c9562d8fd5f301
Karl Brown <karl@cs.stanford.edu>
2022-08-22 12:34:46 -0400

    Refactor note xml generation, separate into functions, pull out some string constants

--- md2enex.py
+++ md2enex.py
@@ -17,7 +17,10 @@ from lxml import etree
 
 APP_NAME = 'md2enex'
 APP_VERSION = '1.0'
-IMPORT_TAG = APP_NAME + '-import'
+IMPORT_TAG_WITH_DATETIME = APP_NAME + '-import' + ":" + datetime.datetime.now().isoformat(timespec='seconds')
+ENEX_DOCTYPE = '<!DOCTYPE en-export SYSTEM "http://xml.evernote.com/pub/evernote-export4.dtd">'
+ENML_PREFIX = '''<?xml version="1.0" encoding="UTF-8" standalone="no"?>
+                 <!DOCTYPE en-note SYSTEM "http://xml.evernote.com/pub/enml2.dtd">'''
 
 
 # stolen from https://stackoverflow.com/a/39501288/4907881
@@ -26,7 +29,7 @@ def creation_date_seconds(path_to_file):
     """
     Try to get the date that a file was created, falling back to when it was
     last modified if that isn't possible.
-    See http://stackoverflow.com/a/39501288/1709587 for explanation.
+    See https://stackoverflow.com/a/39501288/1709587 for explanation.
     """
     if platform.system() == 'Windows':
         return os.path.getctime(path_to_file)
@@ -40,54 +43,77 @@ def creation_date_seconds(path_to_file):
             return stat.st_mtime
 
 
-def process_note(file: str) -> etree.Element:
-    print('processing ' + file, flush=True)
+def create_title(file: str) -> etree.Element:
     title = Path(file).stem
+    title_el = etree.Element('title')
+    # just in case, per DTD, title must have no spaces or line endings
+    title_el.text = title.strip()
+    return title_el
+
+
+def create_creation_date(file: str) -> etree.Element:
     creation_date_ts = creation_date_seconds(file)
     creation_date = enex_date_format(datetime.datetime.fromtimestamp(creation_date_ts, tz=datetime.timezone.utc))
+    created_el = etree.Element('created')
+    created_el.text = creation_date
+    return created_el
+
+
+def create_updated_date(file: str) -> etree.Element:
     modification_date_ts = os.path.getmtime(file)
     modification_date = enex_date_format(datetime.datetime.fromtimestamp(modification_date_ts,
                                                                          tz=datetime.timezone.utc))
-
-    note_el = etree.Element('note')
-
-    title_el = etree.SubElement(note_el, 'title')
-    # just in case, per DTD, title must have no spaces or line endings
-    title_el.text = title.strip()
-
-    content_text = ''
-    html_text = pypandoc.convert_file(file, 'html', format='markdown+hard_line_breaks', extra_args=['--wrap=none'])
-    for index, line in enumerate(html_text.splitlines()):
-        # skip h1 tag from first line, if present
-        if index == 0 and line.strip().startswith('<h1'):
-            continue
-        content_text += line.strip()
-
-    content_el = etree.SubElement(note_el, 'content')
-    enml_prefix = '''<?xml version="1.0" encoding="UTF-8" standalone="no"?>
-    <!DOCTYPE en-note SYSTEM "http://xml.evernote.com/pub/enml2.dtd">'''
-    # Create en-note by hand, as otherwise lxml will escape the content.
-    # Add 6 spaces at the end to match Evernote output
-    enml = enml_prefix + '<en-note>' + content_text + '</en-note>' + '      '
-    content_el.text = etree.CDATA(enml)
-
-    created_el = etree.SubElement(note_el, 'created')
-    created_el.text = creation_date
-
-    updated_el = etree.SubElement(note_el, 'updated')
+    updated_el = etree.Element('updated')
     updated_el.text = modification_date
+    return updated_el
 
-    # Add tags to be able to view imported notes more easily -
-    # one top-level tag, and a second with a date string
-    tag_1_el = etree.SubElement(note_el, 'tag')
-    tag_1_el.text = IMPORT_TAG
 
-    tag_2_el = etree.SubElement(note_el, 'tag')
-    tag_2_el.text = IMPORT_TAG + ":" + datetime.datetime.now().isoformat(timespec='seconds')
+def create_tag() -> etree.Element:
+    tag_el = etree.Element('tag')
+    tag_el.text = IMPORT_TAG_WITH_DATETIME
+    return tag_el
 
-    note_attributes_el = etree.SubElement(note_el, 'note-attributes')
+
+def create_note_attributes() -> etree.Element:
+    note_attributes_el = etree.Element('note-attributes')
     # to make format match standard Evernote export
     note_attributes_el.text = os.linesep
+    return note_attributes_el
+
+
+def create_note_content(file: str) -> etree.Element:
+    content_text = ''
+    # set hard_line_breaks here b/c the Exporter on OSX doesn't add proper line breaks in the Markdown export
+    html_text = pypandoc.convert_file(file, 'html', format='markdown+hard_line_breaks', extra_args=['--wrap=none'])
+    for index, line in enumerate(html_text.splitlines()):
+        line_trimmed = line.strip()
+        # skip h1 tag from first line, if present, as this is likely the title
+        if index == 0 and line_trimmed.startswith('<h1'):
+            continue
+        if line_trimmed.startswith('<figure') or line_trimmed.startswith('<img'):
+            print("Skipped " + file + " because of unsupported figure/image tags.")
+            return None
+        content_text += line_trimmed
+
+    # Create en-note by hand, as otherwise lxml will escape the content.
+    # Add 6 spaces at the end to match Evernote output
+    enml = ENML_PREFIX + '<en-note>' + content_text + '</en-note>' + '      '
+
+    content_el = etree.Element('content')
+    content_el.text = etree.CDATA(enml)
+    return content_el
+
+
+def process_note(file: str) -> etree.Element:
+    print('processing ' + file, flush=True)
+    note_el = etree.Element('note')
+
+    note_el.append(create_title(file))
+    note_el.append(create_note_content(file))
+    note_el.append(create_creation_date(file))
+    note_el.append(create_updated_date(file))
+    note_el.append(create_tag())
+    note_el.append(create_note_attributes())
 
     return note_el
 
@@ -100,13 +126,8 @@ def enex_date_format(date: datetime) -> str:
     return date_str
 
 
-def make_enex_doctype() -> str:
-    doctype = '<!DOCTYPE en-export SYSTEM "http://xml.evernote.com/pub/evernote-export4.dtd">'
-    return doctype
-
-
 # header material for enex format
-def make_en_export() -> etree.Element:
+def create_en_export() -> etree.Element:
     now = datetime.datetime.now(datetime.timezone.utc)
     now_str = enex_date_format(now)
     en_export = etree.Element('en-export')
@@ -124,14 +145,13 @@ def make_enex(target_directory: str, output_file: str):
         sys.exit('No markdown files found in ' + target_directory)
 
     # ElementTree object that will contain our xml
-    root = make_en_export()
+    root = create_en_export()
 
     for count, file in enumerate(files, start=1):
         root.append(process_note(file))
 
     tree = etree.ElementTree(root)
-    doctype = make_enex_doctype()
-    tree.write(output_file, encoding="UTF-8", method='xml', pretty_print=True, xml_declaration=True, doctype=doctype)
+    tree.write(output_file, encoding="UTF-8", method='xml', pretty_print=True, xml_declaration=True, doctype=ENEX_DOCTYPE)
 
     print('Successfully wrote ' + str(count) + ' markdown files to ' + os.path.join(target_directory, output_file))
 
